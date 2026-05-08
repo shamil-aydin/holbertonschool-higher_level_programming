b7d1c9249ef80e35e597793814bcad8974cec4e3
shamil-aydin <13754@holbertonstudents.com>
2026-05-08 13:29:38 +0000

    Fix PEP 8: E501 line too long in Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
     if not a_dictionary:
         return None
-    # max() funksiyası lüğətin dəyərlərinə görə (get metodu ilə) ən böyük açarı tapır
+    # Find the key with the maximum value
     return max(a_dictionary, key=a_dictionary.get)
