6a839e199485711426107cc7a1f8a38141aea653
shamil-aydin <13754@holbertonstudents.com>
2026-05-08 15:14:09 +0000

    Add task 0 - Safe print list

--- python-classes/0-safe_print_list.py
+++ python-classes/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list and returns the real number printed."""
     count = 0
     for i in range(x):
         try:
