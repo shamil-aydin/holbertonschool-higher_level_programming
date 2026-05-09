e20dfacf23a89e66883cf8ecc55f40adebf4525e
shamil-aydin <13754@holbertonstudents.com>
2026-05-09 13:47:38 +0000

    Task 0: fix docstring and readme

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Module for safe list printing."""
-
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list safely.
-
-    Args:
-        my_list (list): The list to print from.
-        x (int): Number of elements to print.
-
-    Returns:
-        int: The real number of elements printed.
-    """
+    """Prints x elements of a list."""
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
         except IndexError:
             break
-    print()
+    print("")
     return count
