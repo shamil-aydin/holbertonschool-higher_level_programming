2d57ad1d4eccf0581258141ad762d8994d978a02
shamil-aydin <13754@holbertonstudents.com>
2026-05-10 12:26:44 +0000

    Fix task 2 - handle missing argv

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,9 +4,10 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+if len(sys.argv) > 2:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data) as response:
-    print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
