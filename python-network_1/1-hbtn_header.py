05c0586ae20c48ad07ae43d69d1b849ea673c7a5
shamil-aydin <13754@holbertonstudents.com>
2026-05-10 12:24:52 +0000

    Fix task 1 - shorten docstring

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that takes a URL, sends a request and displays X-Request-Id header."""
+"""Sends a request to a URL and displays the X-Request-Id header."""
 import urllib.request
 import sys
 
