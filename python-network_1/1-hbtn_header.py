f771880b9617fc3c9fb1b53ba751a4187dd5ef9f
shamil-aydin <13754@holbertonstudents.com>
2026-05-10 12:22:49 +0000

    Fix task 1 - handle missing argv

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@
 import urllib.request
 import sys
 
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+if len(sys.argv) > 1:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
